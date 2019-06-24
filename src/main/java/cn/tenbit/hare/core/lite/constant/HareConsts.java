1b9301dfffd17e8fac6e2df1bd586eda2ef90a58
bangquan.qian <chainz@foxmail.com>
2019-06-24 13:38:45 +0800

    commit

---
+++ src/main/java/cn/tenbit/hare/core/lite/constant/HareConsts.java
@@ -0,0 +1,24 @@
+package cn.tenbit.hare.core.lite.constant;
+
+import org.apache.commons.lang3.StringUtils;
+
+/**
+ * @Author bangquan.qian
+ * @Date 2019-06-24 13:17
+ */
+public interface HareConsts {
+
+    String EMPTY = StringUtils.EMPTY;
+
+    String SPACE = StringUtils.SPACE;
+
+    String NEW_LINE = System.getProperty("line.separator", "\n");
+
+    String UNDER_SCORE = "_";
+
+    String COMMA = ",";
+
+    String SHAPE = "#";
+
+    String PRINT_TIME_PATTERN = "[yyyy-MM-dd HH:mm:ss.SSS]";
+}
---
+++ src/main/java/cn/tenbit/hare/core/lite/exception/HareException.java
@@ -0,0 +1,8 @@
+package cn.tenbit.hare.core.lite.exception;
+
+/**
+ * @Author bangquan.qian
+ * @Date 2019-06-24 13:29
+ */
+public class HareException {
+}
