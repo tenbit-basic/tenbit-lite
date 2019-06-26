c4723c1fe3df978878f22e9e6d0899ec0794d180
bangquan.qian <chainz@foxmail.com>
2019-06-26 14:40:57 +0800

    Create HareExecutor.java

---
+++ src/main/java/cn/tenbit/hare/core/lite/function/HareExecutor.java
@@ -0,0 +1,11 @@
+package cn.tenbit.hare.core.lite.function;
+
+/**
+ * @Author bangquan.qian
+ * @Date 2019-06-26 14:39
+ */
+@FunctionalInterface
+public interface HareExecutor {
+
+    void execute();
+}
