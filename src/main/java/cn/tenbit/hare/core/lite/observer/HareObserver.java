a74b59a2979c373d9cebb18dd2b96396ec804a6a
bangquan.qian <chainz@foxmail.com>
2019-07-21 14:27:08 +0800

    Update HareObserver.java

--- src/main/java/cn/tenbit/hare/core/lite/observer/HareObserver.java
+++ src/main/java/cn/tenbit/hare/core/lite/observer/HareObserver.java
@@ -4,7 +4,7 @@ package cn.tenbit.hare.core.lite.observer;
  * @Author bangquan.qian
  * @Date 2019-07-21 13:55
  */
-public interface HareObserver extends Comparable {
+public interface HareObserver<T> extends Comparable<T> {
 
     void update(HareObservable observable, Object arg);
 }
